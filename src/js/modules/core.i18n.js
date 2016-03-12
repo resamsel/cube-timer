8d2e32ee94c1c3c114cbf4a5722d1afca4498554
René Samselnig <Rene.Samselnig@skiline.cc>
2016-03-12 22:56:39 +0100

    Chg: Make grunt dist work for webapp and Chrome app

--- src/js/modules/core.i18n.js
+++ src/js/modules/core.i18n.js
@@ -26,7 +26,7 @@ Core.register(
 
         module.loadMessages = function(locale) {
             $.ajax({
-                url: '_locales/' + locale + '/messages.json',
+                url: 'locales/' + locale + '/messages.json',
                 dataType: 'json'
             }).done(function (data) {
                 I18n.messages = data;
